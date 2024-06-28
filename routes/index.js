0ef433a333fe8a72254adcec82eb71c8dd0b3fbd
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-06-28 22:45:29 +0200

    Add user routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const characterController = require("../controllers/characterController");
+const userController = require("../controllers/userController");
 
 router.get("/characters", characterController.characters);
 router.post("/check", characterController.check);
 router.post("/checkWin", characterController.checkWin);
-router.post("/user", (req, res) => {
-  res.json("hello");
-});
+router.get("/start", userController.setTimer);
+router.get("/stop", userController.stopTimer);
+router.post("/save", userController.save);
 
 module.exports = router;
