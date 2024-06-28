67766e23481d79adfc2ff8b21a7394b5a5e0b846
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-06-28 19:08:32 +0200

    Fix cors

--- app.js
+++ app.js
@@ -14,13 +14,12 @@ async function main() {
 const indexRouter = require("./routes/index");
 
 const app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 app.use("/", indexRouter);
 
