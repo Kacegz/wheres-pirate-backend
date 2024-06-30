8b5ef12d14aa652fe0470e57ff6afb7af5d1e2b7
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-06-30 22:27:08 +0200

    Add validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,18 @@
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
-const { differenceInMilliseconds, format } = require("date-fns");
+const { differenceInMilliseconds } = require("date-fns");
 exports.setTimer = asyncHandler(async (req, res) => {
-  req.session.userStart = new Date();
-  console.log(req.session);
-  return res.json(true);
+  if (!req.session.started) {
+    req.session.started = true;
+    req.session.userStart = new Date();
+    return res.json(true);
+  }
+  return res.json(req.session.userStart);
 });
 exports.stopTimer = asyncHandler(async (req, res) => {
   req.session.userFinish = new Date();
-  console.log(req.session);
+  req.session.started = false;
   res.json({
     time: differenceInMilliseconds(
       req.session.userFinish,
@@ -17,22 +20,29 @@ exports.stopTimer = asyncHandler(async (req, res) => {
     ),
   });
 });
-exports.save = asyncHandler(async (req, res) => {
-  const exists = await User.findOne({ nickname: req.body.nickname }).exec();
-  if (!exists) {
-    const newUser = new User({
-      nickname: req.body.nickname,
-      time: differenceInMilliseconds(
-        req.session.userFinish,
-        req.session.userStart
-      ),
-    });
-    newUser.save();
-    res.json(newUser);
-  } else {
-    res.json({ error: "user already exists" });
-  }
-});
+exports.save = [
+  body("nickname", "Nickname must not be empty").isLength({ min: 1 }).escape(),
+  asyncHandler(async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.json({ error: result.array()[0].msg });
+    }
+    const exists = await User.findOne({ nickname: req.body.nickname }).exec();
+    if (!exists) {
+      const newUser = new User({
+        nickname: req.body.nickname,
+        time: differenceInMilliseconds(
+          req.session.userFinish,
+          req.session.userStart
+        ),
+      });
+      newUser.save();
+      res.json(newUser);
+    } else {
+      res.json({ error: "user already exists" });
+    }
+  }),
+];
 exports.leaderboard = asyncHandler(async (req, res) => {
   const top = await User.find({}).sort({ time: 1 }).limit(10).exec();
   res.json(top);
